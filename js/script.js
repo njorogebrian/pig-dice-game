57ed6a0f9dfe239b0d856820535844f2e25539fc
njorogebrian <briannjoroge2000@gmail.com>
2019-05-13 11:44:06 +0300

    add fuctions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(event){
     $(".pig").hide(6000);
 });
+    var one, two, three, four
+    one = 0
+    two = 0
+    three = 0
+    four = 0
+    $(document).ready(function(event){
+        $("#btn").click(function(){
+            var numbers = Math.floor(Math.random()*6)+1;
+            if( numbers === 1){
+                alert("you've rolled one")
+                one=0;
+                two += one;
+                $("#cum1").text(two);
+            }
+            else{
+                $("#ini1").text(numbers)
+                one+=numbers;
+                $("#cum1").text(one);
+            }
+        })
+    })
